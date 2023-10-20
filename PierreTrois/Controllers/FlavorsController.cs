dd9d344001ed7357ba5e917bd43bc2829640930d
Paul LeTourneau <pletourneau62@gmail.com>
2023-10-20 12:32:51 -0700

    add flavor controller

---
+++ PierreTrois/Controllers/FlavorsController.cs
@@ -0,0 +1,122 @@
+using Microsoft.AspNetCore.Mvc;
+using PierreTrois.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PierreTrois.Controllers
+{
+  [Authorize]
+  public class FlavorsController : Controller
+  {
+    private readonly PierreTroisContext _db;
+
+    public FlavorsController(PierreTroisContext db)
+    {
+      _db = db;
+    }
+
+    [AllowAnonymous]
+    public ActionResult Index()
+    {
+      ViewBag.Name = "List of Flavors";
+      return View(_db.Flavors.ToList());
+    }
+  
+    public ActionResult Create()
+    {
+      ViewBag.Name = "Add a new flavor";
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor flavor)
+    {
+      if (!ModelState.IsValid)
+      {
+        ViewBag.Name = "Add a new flavor";
+        return View(flavor);
+      }
+      else
+      {
+        _db.Flavors.Add(flavor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    [AllowAnonymous]
+    public ActionResult Details(int id)
+    {
+      ViewBag.Name = "Flavor Details";
+      Flavor targetFlavor = _db.Flavors.Include(entry => entry.JoinEntities)
+                                              .ThenInclude(join => join.Treat)
+                                              .FirstOrDefault(entry => entry.FlavorId == id);
+      return View(targetFlavor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      ViewBag.Name = "Edit Flavor";
+      Flavor targetFlavor = _db.Flavors.FirstOrDefault(entry => entry.FlavorId == id); 
+      return View(targetFlavor);     
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavorToEdit)
+    {
+      _db.Flavors.Update(flavorToEdit);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = flavorToEdit.FlavorId });
+    }
+
+    public ActionResult Delete(int id)
+    {
+      ViewBag.Name = "Delete Flavor";
+      Flavor targetFlavor = _db.Flavors.FirstOrDefault(entry => entry.FlavorId == id);
+      return View(targetFlavor);            
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Flavor targetFlavor = _db.Flavors.FirstOrDefault(entry => entry.FlavorId == id);
+      _db.Flavors.Remove(targetFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    
+    public ActionResult AddTreat(int id)
+    {
+      ViewBag.Name = "Add Treat to this Flavor";
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(model => model.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+      return View(thisFlavor);
+    }
+
+    [HttpPost]
+    public ActionResult AddTreat(Flavor flavor, int treatId)
+    {
+      #nullable enable
+      TreatFlavor? joinEntity = _db.TreatFlavors.FirstOrDefault(join =>(join.TreatId == treatId && join.FlavorId == flavor.FlavorId));
+      #nullable disable
+      if (joinEntity == null && treatId != 0)
+      {
+        _db.TreatFlavors.Add(new TreatFlavor() {TreatId = treatId, FlavorId = flavor.FlavorId});
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new {id = flavor.FlavorId});
+    }
+
+    [HttpPost]
+    public ActionResult DeleteJoin(int joinId)
+    {
+      TreatFlavor joinEntry = _db.TreatFlavors.FirstOrDefault(entry => entry.TreatFlavorId == joinId);
+      _db.TreatFlavors.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.FlavorId });
+    }
+  }
+}
